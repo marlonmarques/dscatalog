b6aa17e4c31ca60ea2f8e3aa5ae921821d669be7
Marlon Marques <marlonmarques6@gmail.com>
2025-10-27 20:09:51 -0300

    category resource

---
+++ backend/src/main/java/com/dvsuperior/dscatalog/entities/Category.java
@@ -0,0 +1,54 @@
+package com.dvsuperior.dscatalog.entities;
+
+import java.util.Objects;
+
+public class Category {
+
+    private Long id;
+    private String name;
+
+
+    public Category() {
+    }
+
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Category)) {
+            return false;
+        }
+        Category category = (Category) o;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+    
+}
