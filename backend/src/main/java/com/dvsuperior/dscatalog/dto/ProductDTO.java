d308217f9d7cd80ff4312b7a673e89d6c5796208
Marlon Marques <marlonmarques6@gmail.com>
2025-10-30 18:02:14 -0300

    Product repository, service, get ok

---
+++ backend/src/main/java/com/dvsuperior/dscatalog/dto/ProductDTO.java
@@ -0,0 +1,109 @@
+package com.dvsuperior.dscatalog.dto;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import com.dvsuperior.dscatalog.entities.Category;
+import com.dvsuperior.dscatalog.entities.Product;
+
+
+public class ProductDTO {
+
+    private Long id;
+    private String name;
+    private Double price;
+    private String description;
+    private String imgUrl;
+    private Instant date;
+
+    private List<CategoryDTO> categories = new ArrayList<>();
+
+    public ProductDTO() {
+    }
+
+
+    public ProductDTO(Long id, String name, Double price, String description, String imgUrl, Instant date) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.imgUrl = imgUrl;
+        this.date = date;
+    }
+
+     public ProductDTO(Product entity) {
+        id = entity.getId();
+        name = entity.getName();
+        price = entity.getPrice();
+        description = entity.getDescription();
+        imgUrl = entity.getImgUrl();
+        date = entity.getDate();
+    }
+
+    public ProductDTO(Product entity, Set<Category> categories) {
+        this(entity);
+        categories.forEach(cat -> this.categories.add(new CategoryDTO(cat)));
+    }
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImgUrl() {
+        return this.imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public Instant getDate() {
+        return this.date;
+    }
+
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    public List<CategoryDTO> getCategories() {
+        return this.categories;
+    }
+
+    public void setCategories(List<CategoryDTO> categories) {
+        this.categories = categories;
+    }
+
+
+
+}
